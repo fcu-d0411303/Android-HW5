61c7245004e204c7aa87dd0f2e53c8ecb1fb584f
user <https://github.com/fcu-d0411303/Android-HW5.git>
2017-04-25 21:14:44 +0800

    hw5 1

---
+++ app/src/main/java/com/example/user/myapplication/picture.java
@@ -0,0 +1,17 @@
+package com.example.user.myapplication;
+
+/**
+ * Created by user on 2017/4/25.
+ */
+
+public class picture {
+    String name;
+    int imgid;
+    int num;
+
+    picture(String name, int imgid, int num) {
+        this.imgid;
+        this.name;
+        this.num;
+    }
+}
